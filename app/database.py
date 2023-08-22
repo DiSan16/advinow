b44e8764791df177b03dab89d9dd84ecb9a62e73
SanchitD <79684090+Siniade@users.noreply.github.com>
2023-08-22 13:15:52 -0400

    Second commit

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Replace this with the desired path for your SQLite database file
-DATABASE_URL = "sqlite:///./symptomData.db"
+DATABASE_URL = "postgresql://postgre:Sanchitpsql@localhost/adviNow"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from . import models
+from .database import SessionLocal
+import csv
 
 router = APIRouter()
 
@@ -8,5 +12,36 @@ async def get_status():
     try:
         return {"Health OK"}
 
+    except Exception as e:
+        return {'Error: ' + str(e)}
+    
+
+
+@router.post("/import-csv/")
+async def import_csv():
+    try:
+        csv_file_path = "app/data/business_symptom_data.csv"
+        db = SessionLocal()
+
+        with open(csv_file_path, mode="r", encoding="utf-8") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            header = next(csv_reader)  # Read the header line
+
+            for row in csv_reader:
+                # Process each row of data here
+                fields = row.split(",")
+                business_id, business_name, symptom_code, symptom_name, symptom_diagnostic = fields
+                symptom = Symptom(business_id=business_id,
+                                business_name=business_name,
+                                code=symptom_code,
+                                name=symptom_name,
+                                diagnostic=symptom_diagnostic)
+
+                db.add(symptom)
+            db.commit()
+            db.close()
+
+        return {"message": "CSV file processed successfully"}
+    
     except Exception as e:
         return {'Error: ' + str(e)}
